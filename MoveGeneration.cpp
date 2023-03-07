277dd97e657bce5ccf59cce913eeb6c107a374d4
Jeb Barker <48389379+jeb-barker@users.noreply.github.com>
2023-03-07 11:40:12 -0500

    Initial Commit

---
+++ MoveGeneration.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Jeb Barker on 3/7/23.
+//
+#include "MoveGeneration.h"
+
+uint64_t whiteKingMoves(uint64_t& kingPos, uint64_t& blackPieces, uint64_t& whitePieces, uint64_t& specialFlags)
+{
+
+}
